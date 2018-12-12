206c33f47bd1a3643f6cdc5f4d51f54bc394b228
Andrew Lvovsky <alvov001@ucr.edu>
2018-12-11 18:01:51 -0800

    refactored code for reading file into its own function

--- main.m
+++ main.m
@@ -1,28 +1,31 @@
 % Project 2 
 
-% function main
+function main
 disp("Welcome to Andrew Lvovsky's Nearest Neighbor Feature Selection.");
 prompt = 'Type in the name of the file containing your dataset: ';
 
 filename = input(prompt, 's');
+A = read_data(filename);
+end
+
+% Reads a dataset from a specified file, outputs an m x n matrix
+function A = read_data(filename)
 fileID = fopen(filename);
 if filename == "small86.txt"
     sizeA = [11 200];
 elseif filename == "large4.txt"
     sizeA = [101 200];
 else
-    row_prompt = 'How many rows in your dataset?';
+    row_prompt = 'How many rows in your dataset? ';
     row_num = input(row_prompt);
-    col_prompt = 'How many columns in your dataset?';
+    col_prompt = 'How many columns in your dataset? ';
     col_num = input(col_prompt);
     sizeA = [col_num row_num];
 end
 A = fscanf(fileID, '%f', sizeA);
 fclose(fileID);
-A = A';
-
-
-% end
+A = A'; % transposing A since input read is put in col x row
+end
 
 function y = nearest_neighbor(x)
 % TODO
