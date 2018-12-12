5919b76117f392271dffdb61e7579d68895c0a8d
Andrew Lvovsky <alvov001@ucr.edu>
2018-12-11 21:29:30 -0800

    implemented get_distance function

--- main.m
+++ main.m
@@ -27,9 +27,10 @@ fclose(fileID);
 A = A'; % transposing A since input read is put in col x row matrix
 end
 
+% Returns the nearest member to the current member in dataset A
 function nearest_member_index = nearest_neighbor(A, member_index)
 min_distance = Inf;
-for i = 1:size(A(:,1))
+for i = 1 : size(A(:,1))  % for all rows (members)
     if member_index ~= i
         new_distance = get_distance(member_index, i);
         if new_distance < min_distance
@@ -39,5 +40,12 @@ for i = 1:size(A(:,1))
 end
 end
 
-function distance = get_distance(origin, far_point, n)
+% Computes and returns the Euclidian distance for n-dimensions, where n is
+% the number of columns in dataset A, minus the features column
+function distance = get_distance(A, origin_point, other_point)
+distance = 0;
+for i = 2 : size(A(1,:))  % for all columns (features), skipping members col
+    distance = distance + ( A(other_point, i) - A(origin_point, i) )^2;
+end
+distance = sqrt(distance);
 end
