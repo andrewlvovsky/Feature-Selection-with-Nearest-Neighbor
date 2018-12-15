b6de6d5308f8cb9f32d6955ce0df31756a24cc64
Andrew Lvovsky <alvov001@ucr.edu>
2018-12-14 19:56:26 -0800

    implemented backward elimination

--- main.m
+++ main.m
@@ -1,18 +1,21 @@
-% Project 2 
+% Project 2
 
 function main()
 % disp("Welcome to Andrew Lvovsky's Nearest Neighbor Feature Selection.");
 % prompt = 'Type in the name of the file containing your dataset: ';
 
 % filename = input(prompt, 's');
-data = read_data('small86.txt');
+data = read_data('large4.txt');
 % features = [];
 % accuracy = nearest_neighbor(data, features) * 100;
-[accuracy, best_feature_set] = forward_selection(data);
+% [accuracy, best_feature_set] = forward_selection(data);
+% disp(best_feature_set);
+% disp(['Feature set [', num2str(best_feature_set), ...
+%     '] was best at an accuracy of ', num2str(accuracy), '%.']);
+[accuracy, best_feature_set] = backward_elim(data);
 disp(best_feature_set);
 disp(['Feature set [', num2str(best_feature_set), ...
     '] was best at an accuracy of ', num2str(accuracy), '%.']);
-% disp(['Accuracy: ', num2str(accuracy)]);
 end
 
 % Reads a dataset from a specified file, outputs an m x n matrix (data)
@@ -36,30 +39,91 @@ fclose(fileID);
 data = data'; % transposing data since input is put in col x row matrix
 end
 
+% Returns the most accurate features out of all of th
 function [best_accuracy, best_feature_set] = forward_selection(data)
+num_of_features = get_num_of_features(data);
 features = [];
 best_accuracy = 0;
-for i = 1 : size(data, 2) - 1
+for i = 1 : num_of_features
     disp(" ");
     disp(['On level ', num2str(i), ' of the search tree...']);
     disp(" ");
-    for j = 1 : size(data,2) - 1
+    for j = 1 : num_of_features
         if isempty(intersect(features,j))
             features = [features, j];  % adding feature
             test_accuracy = nearest_neighbor(data, features);
-            disp(['Testing feature set [', num2str(features), ...
-                '] with accuracy: ', num2str(test_accuracy), '%'])
-            features(end) = [];
+            disp(['Tested feature set [', num2str(features), ...
+                '] with accuracy: ', num2str(test_accuracy), '%']);
+            features(end) = []; % removing feature
             if test_accuracy > best_accuracy
                 best_accuracy = test_accuracy;
                 feature_to_add = j;
                 best_feature_set = [features, j];
-                disp(['On level ', num2str(i),', feature ', ... 
-                    num2str(feature_to_add), ' was added to best set!'])
+                disp(['On level ', num2str(i),', feature ', ...
+                    num2str(feature_to_add), ' was added to best set!']);
             end
         end
     end
     features = best_feature_set;
+end
+end
+
+function [overall_best_accuracy, overall_best_feature_set] = backward_elim(data)
+num_of_features = get_num_of_features(data);
+features = zeros(1, num_of_features);   % preallocates features array
+overall_best_accuracy = 0;
+for i = 1 : num_of_features    % populates array with all features
+    features(i) = i;
+end
+test_accuracy = nearest_neighbor(data, features);
+disp(['Tested full feature set [', num2str(features), ...
+    '] with accuracy: ', num2str(test_accuracy), '%']);
+overall_best_accuracy = test_accuracy;
+overall_best_feature_set = features;
+while ~isempty(features)
+    if size(features,2) > 1
+        current_best_accuracy = 0;
+        current_best_feature_set = [];
+        for i = 1 : size(features,2)
+            removed_feature = features(i);
+            features(i) = [];   % remove feature from array
+            test_accuracy = nearest_neighbor(data, features);
+            disp(['Removed feature ', num2str(removed_feature), ...
+                ' from feature set [', num2str(features), ...
+                '] with accuracy: ', num2str(test_accuracy), '%']);
+            if test_accuracy > current_best_accuracy
+                current_best_accuracy = test_accuracy;
+                current_best_feature_set = features;
+                disp(['Feature set [', ...
+                    num2str(current_best_feature_set), ...
+                    '] is local best set, with accuracy ', ...
+                    num2str(current_best_accuracy), '%']);
+                if current_best_accuracy > overall_best_accuracy
+                    overall_best_accuracy = test_accuracy;
+                    overall_best_feature_set = features;
+                    disp(['Feature set [', ...
+                        num2str(overall_best_feature_set), ...
+                        '] is global best set, with accuracy ', ...
+                        num2str(overall_best_accuracy), '%']);
+                end
+            end
+            % add feature back
+            features = [features(1:i-1), removed_feature, features(i:end)];
+        end
+        features = current_best_feature_set;
+    else
+        removed_feature = features;
+        features = [];   % remove feature from array
+        test_accuracy = nearest_neighbor(data, features);
+        if current_best_accuracy > overall_best_accuracy
+            overall_best_accuracy = test_accuracy;
+            overall_best_feature_set = features;
+            disp(['Feature set [', ...
+                num2str(overall_best_feature_set), ...
+                '] is global best set, with accuracy ', ...
+                num2str(overall_best_accuracy), '%']);
+        end
+    end
 end
 end
 
@@ -99,7 +163,7 @@ end
 distance = sqrt(distance);
 end
 
-% Test function to see if searches work
-% function accuracy = leave_one_out_cross_val(data, features, feature_to_add)
-% accuracy = rand;
-% end
+function num_of_features = get_num_of_features(data)
+num_of_features = size(data(1,:));
+num_of_features = num_of_features(2) - 1;
+end
